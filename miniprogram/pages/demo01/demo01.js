51d863a47934561b9bcc8adefdf3b582ec36cdb3
Lee8150951 <Liys9875@outlook.com>
2021-03-10 16:45:25 +0800

    day01.2

---
+++ miniprogram/pages/demo01/demo01.js
@@ -0,0 +1,37 @@
+//Page Object
+Page({
+  data: {
+    msg: "数据绑定 Hello World",
+    number: 10000,
+    boolean: false,
+    person: {
+      age: 23,
+      height: 173,
+      weight: 127,
+      name: 'jacob'
+    },
+    isChecked: true,
+    list: [
+      {
+        id: 0,
+        name: 'abb'
+      },
+      {
+        id: 1,
+        name: 'bcc'
+      },
+      {
+        id: 2,
+        name: 'cdd'
+      },
+      {
+        id: 3,
+        name: 'dee'
+      },
+      {
+        id: 4,
+        name: 'eef'
+      },
+    ]
+  }
+});
